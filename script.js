04f12d1849955a28c532300ef0264ee631f91f7c
Angel Sanchez <angel.2525@live.com.mx>
2025-11-19 09:46:34 -0700

    feat: se agregó una sección de código al archivo script.js para recorrer e imprimir los números de 1 al 1000

---
+++ script.js
@@ -0,0 +1,11 @@
+//Angel Sanchez Paez - Generation CH62
+
+// Inicio
+let numeros; //Variable que llevará el conteo de los números del 1 al 1000
+
+for(numeros = 1; numeros <= 1000; numeros++) //Bucle que recorre los números del 1 al 1000
+    {
+    	console.log(numeros);
+    }
+    
+    //Fin
